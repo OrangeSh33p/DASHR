dc118c9506102e26a26720da7e9067b61fc5f8b6
Henri <couvreurhenri@gmail.com>
2019-03-19 13:50:05 +0100

    Initial commit

---
+++ Assets/Follow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+	public Transform target;
+	[Range(0,1)]
+	public float smoothness;
+	Quaternion originRot;
+	Vector3 originPos;
+
+	void Start () {
+		originRot = transform.rotation;
+		originPos = transform.position;
+	}
+
+	void Update () {
+		Vector3 targetPos = originPos + new Vector3 (target.position.x, 0, target.position.z);
+		transform.position = Vector3.Lerp(transform.position, targetPos, 1-smoothness);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation*originRot, smoothness);
+	}
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+	public float rotateSpeed;
+	public float dashLength;
+	public float minSpeed;
+	public float maxSpeed;
+	public float currentSpeed;
+	public float acceleration;
+	public float deceleration;
+	public float stopping;
+
+	void Update () {
+		if (Input.GetKey(KeyCode.Z)) {
+			currentSpeed = Mathf.Min(currentSpeed + acceleration*Time.deltaTime, maxSpeed);
+		} else if (Input.GetKey(KeyCode.S)) {
+			currentSpeed = Mathf.Max(currentSpeed-Time.deltaTime*stopping, minSpeed);
+		}
+		else {
+			currentSpeed = Mathf.Max(currentSpeed-Time.deltaTime*deceleration, minSpeed);
+		}
+		Move ();
+		Rotate();
+		if (Input.GetKeyDown(KeyCode.Q)) DashLeft();
+		if (Input.GetKeyDown(KeyCode.D)) DashRight();
+	}
+
+	void Move () {
+		transform.position += currentSpeed*Time.deltaTime*transform.forward;
+	}
+
+	void Rotate () {
+		transform.rotation *= Quaternion.Euler (0, rotateSpeed*Time.deltaTime*Input.GetAxis("Mouse X"), 0);
+	}
+
+	void Dash (bool isLeft) {
+		int multiplier = isLeft?-1:1;
+		transform.position += multiplier*dashLength*transform.right;
+	}
+
+	void DashLeft () {
+		Dash (true);
+	}
+
+	void DashRight () {
+		Dash(false);
+	}
+}
